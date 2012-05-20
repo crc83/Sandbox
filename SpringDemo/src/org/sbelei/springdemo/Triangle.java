dd754ce64f099132ca8d81300219dab94225b23b
Sergiy Beley <sergiy.beley@gmail.com>
2012-05-20 18:25:26 +0300

    Constructor with two parameters

--- SpringDemo/src/org/sbelei/springdemo/Triangle.java
+++ SpringDemo/src/org/sbelei/springdemo/Triangle.java
@@ -3,16 +3,30 @@ package org.sbelei.springdemo;
 public class Triangle {
 	
 	private String type;
+	private int height;
+
+	public Triangle(String aType) {
+		type = aType;
+	}
+
+	public Triangle(String aType, int aHeight) {
+		type = aType;
+		height = aHeight;
+	}
 
 	public String getType() {
 		return type;
 	}
 
-	public void draw(){
-		System.out.println("Triangle of ["+type+"] drawn");
+	public int getHeight() {
+		return height;
 	}
 
-	public void setType(String arg0) {
-		type = arg0;		
+	public void draw(){
+		System.out.println("Triangle of ["+getType()+"] drawn whit height "+getHeight());
 	}
+
+//	public void setType(String arg0) {
+//		type = arg0;		
+//	}
 }
