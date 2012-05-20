5b0fe3ea09d3181235e2a817e79444e89374021a
Sergiy Beley <sergiy.beley@gmail.com>
2012-05-20 19:41:18 +0300

    09. Initializing collections

--- SpringDemo/src/org/sbelei/springdemo/Triangle.java
+++ SpringDemo/src/org/sbelei/springdemo/Triangle.java
@@ -1,39 +1,25 @@
 package org.sbelei.springdemo;
 
+import java.util.List;
+
 public class Triangle {
 
-	private Point pointA;
-	private Point pointB;
-	private Point pointC;
-
-	public Point getPointA() {
-		return pointA;
-	}
-
-	public void setPointA(Point pointA) {
-		this.pointA = pointA;
-	}
-
-	public Point getPointB() {
-		return pointB;
-	}
-
-	public void setPointB(Point pointB) {
-		this.pointB = pointB;
-	}
-
-	public Point getPointC() {
-		return pointC;
-	}
-
-	public void setPointC(Point pointC) {
-		this.pointC = pointC;
-	}
+	private List<Point> points;
 
 	public void draw(){
 		System.out.println("Triangle");
-		System.out.println("Point A ("+getPointA().getX()+";"+getPointA().getY()+")");
-		System.out.println("Point B ("+getPointB().getX()+";"+getPointB().getY()+")");
-		System.out.println("Point C ("+getPointC().getX()+";"+getPointC().getY()+")");
+		char letter = 'A';
+		for (Point point : points){
+			System.out.println("Point "+letter+" ("+point.getX()+";"+point.getY()+")");
+			letter++;
+		}
+	}
+
+	public List<Point> getPoints() {
+		return points;
+	}
+
+	public void setPoints(List<Point> points) {
+		this.points = points;
 	}
 }
