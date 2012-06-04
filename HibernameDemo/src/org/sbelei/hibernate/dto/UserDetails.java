0d5507594d98dd96520ffa68671f6ace3c0dace3
Sergiy Beley <sergiy.beley@gmail.com>
2012-06-04 13:02:04 +0300

    //3.3 Item is stored in database

---
+++ HibernameDemo/src/org/sbelei/hibernate/dto/UserDetails.java
@@ -0,0 +1,46 @@
+package org.sbelei.hibernate.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class UserDetails {
+
+	@Id
+	private int userId;
+	
+	private String userName;
+
+	public UserDetails() {
+
+	}
+
+	/**
+	 * @return the userId
+	 */
+	public int getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
