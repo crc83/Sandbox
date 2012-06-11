214560f60b004a594a2705b789d5ef5dad00a0ae
Sergiy Beley <sergiy.beley@gmail.com>
2012-06-11 10:46:06 +0300

    07 - Generation strategy specified

--- HibernameDemo/src/main/java/org/sbelei/hibernate/dto/UserDetails.java
+++ HibernameDemo/src/main/java/org/sbelei/hibernate/dto/UserDetails.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.Table;
@@ -24,7 +25,7 @@ public class UserDetails {
 	}
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.TABLE)
 	private int userId;
 	private String userName;
 	
